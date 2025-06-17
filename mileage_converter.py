9e4e66f97d8d44ffea2375d5f8f96c4f49949168
jack <whisler1@gmail.com>
2025-06-17 13:31:12 -0500

    rps basics

--- mileage_converter.py
+++ mileage_converter.py
@@ -1,4 +1,4 @@
 print("How many kilometers did you run today?")
 kms = input()
 miles = round((float(kms)) / 1.60934, 2)
-print(f"That is equal to {miles} miles.")
+print(f"{kms}kms is equal to {miles} miles.")
