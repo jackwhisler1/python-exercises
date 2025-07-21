2fa983d3af92507e0fa4c9cd6d45db9652253a69
Jack Whisler <78163023+jackwhisler1@users.noreply.github.com>
2025-07-21 12:12:41 -0500

    leetcode practice

--- practice.py
+++ practice.py
@@ -43,3 +43,41 @@ def calculate(**kwargs):
     else:
         final = f"{kwargs.get('message', 'The result is')} {int(operation_value)}"
     return final
+
+
+def decrement_list(nums):
+    return list(map(lambda num: num - 1, nums))
+
+
+def remove_negatives(nums):
+    return list(filter(lambda num: num >= 0, nums))
+
+
+def is_all_strings(vals):
+    return all(isinstance(val, str) for val in vals)
+
+
+is_all_strings(['a', 'b', 'c'])  # True
+
+print(is_all_strings([2, 'a', 'b', 'c']))  # False
+
+is_all_strings(('hello', 'goodbye'))  # Tru
+
+
+def interleave(word1, word2):
+    pairs = zip(word1, word2)
+    # for pair in pairs:
+    #     ''.join(pair)
+    woven = ''.join([''.join(pair) for pair in pairs])
+    print(list(pairs))
+    print(woven)
+
+
+def triple_and_filter(nums):
+    multiples_of_four = filter(lambda num: num % 4 == 0, nums)
+
+    return [num * 3 for num in multiples_of_four]
+
+
+def friend(x):
+    return [f for f in x if len(f) == 4]
